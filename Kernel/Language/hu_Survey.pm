d50998df17ca196f173a9b2471962766932b7ab6
ZnunyWeblate <operations+weblate@znuny.com>
2026-03-27 09:19:37 +0100

    Updated .pm translation files.

--- Kernel/Language/hu_Survey.pm
+++ Kernel/Language/hu_Survey.pm
@@ -116,7 +116,7 @@ sub Data {
     $Self->{Translation}->{'Yes/No'} = 'Igen/Nem';
     $Self->{Translation}->{'Radio (List)'} = 'Választógomb (lista)';
     $Self->{Translation}->{'Checkbox (List)'} = 'Jelölőnégyzet (lista)';
-    $Self->{Translation}->{'Net Promoter Score'} = 'Valós ügyféltámogatottsági érték';
+    $Self->{Translation}->{'Net Promoter Score'} = 'Valós ügyfél-támogatottsági érték';
     $Self->{Translation}->{'Question Type'} = 'Kérdés típusa';
     $Self->{Translation}->{'Complete'} = 'Kész';
     $Self->{Translation}->{'Incomplete'} = 'Befejezetlen';
